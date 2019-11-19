c8b4967e228601c45d1dce9174f2ca3197e6db6e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-19 16:26:34 +0300

    building models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('S', 'Shirt')
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+
+class OrderItem(models.Model):
+    pass
+
+class Order(models.Model):
+    pass
