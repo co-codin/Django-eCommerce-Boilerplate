68d8801c9d978bd2450384d9227501c9df42ac92
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-19 18:23:15 +0300

    basic template

--- core/models.py
+++ core/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 CATEGORY_CHOICES = (
-    ('S', 'Shirt')
+    ('S', 'Shirt'),
     ('SW', 'Sport wear'),
     ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
 )
 
 class Item(models.Model):
@@ -11,9 +18,25 @@ class Item(models.Model):
     price = models.FloatField()
     discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
 
+    def __str__(self):
+        return self.title
+    
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    item = models.ManyToManyField(Item)
     pass
 
 class Order(models.Model):
-    pass
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.username
